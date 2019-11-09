ac48f374d7fe624360c0f1293c883394b0603f9a
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-09 15:10:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each.with_index(1) {|name, index| puts "#{index} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|calls| calls.capitalize + "!"}
 end
+  
+  
+def long_planeteer_calls(calls)
+  calls.each {|strings| 
+    if strings.length > 4
+      return true 
+    end 
+  }
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+  false
+ end
+ 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each {|strings|
+    if strings.include?(strings)
+      puts strings
+    end
+  }
+  
 end
